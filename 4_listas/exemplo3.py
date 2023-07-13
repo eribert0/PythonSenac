6ce86d22448c88e466a3348066cb4e53e5ecaf8d
Eriberto Junior <43413666+eribert0@users.noreply.github.com>
2023-07-13 17:01:09 -0300

    Create exemplo3.py

---
+++ 4_listas/exemplo3.py
@@ -0,0 +1,11 @@
+carro = {
+    "marca": "Ford",
+    "modelo": "Ka",
+    "ano": 2006,
+    "cor": "prata"
+}
+
+for propriedade in carro:
+    print(propriedade)
+    print(carro[propriedade])
+    print("-----------------------")
