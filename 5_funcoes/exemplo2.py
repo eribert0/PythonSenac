53b1c1d67c162c4a2b716152abe3ab1f1408409c
Eriberto Junior <43413666+eribert0@users.noreply.github.com>
2023-07-14 14:45:11 -0300

    Create exemplo2.py

---
+++ 5_funcoes/exemplo2.py
@@ -0,0 +1,10 @@
+def questao1(numero):
+    resultado = ""
+    for n in range(1, numero + 1):
+        for i in range (1, n + 1):
+            resultado += f"{str(n)}"
+        resultado += "\n"
+    return resultado
+
+print(questao1(9))
+    
