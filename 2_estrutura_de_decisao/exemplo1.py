e6607312115554cd5bec24da23f929b0be6a8830
Eriberto Junior <43413666+eribert0@users.noreply.github.com>
2023-07-17 23:15:24 -0300

    Update exemplo1.py

--- 2_estrutura_de_decisao/exemplo1.py
+++ 2_estrutura_de_decisao/exemplo1.py
@@ -8,8 +8,8 @@ media = (nota1 + nota2 + nota3)/3.0;
 
 print(f"A média é igual a {media}");
 
-aprovado = media >=6.0;
-reprovado = media <=3.0;
+aprovado = media >= 6.0;
+reprovado = media <= 3.0;
 
 if aprovado:
     print("Você está aprovado");
