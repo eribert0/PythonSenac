dd5ce2026e0d95d7e42f60e384fc14b7acf288c3
Eriberto Junior <43413666+eribert0@users.noreply.github.com>
2023-07-18 02:33:12 -0300

    Create ex1.py

---
+++ 1_estrutura_sequencial/ex1.py
@@ -0,0 +1,22 @@
+"""
+numero1 = int(input("Digite o primeiro numero: "))
+
+numero2 = int(input("Digite o segundo numero: "))
+
+soma = numero1 + numero2
+
+print(f"A soma entre {numero1} e {numero2} é {soma}")
+"""
+nome = 'Roger'
+sobrenome = 'Guedes'
+idade = 25
+ano_de_nascimento = 2023 - idade
+maior_de_idade = idade >= 18
+altura_metros = 1.79
+
+print("Nome: ", nome)
+print("Sobrenome: ", sobrenome)
+print("Idade: ", idade)
+print("Ano de nascimento: ", ano_de_nascimento)
+print("É maior de idade?", maior_de_idade)
+print('Altura em metros: ', altura_metros)
