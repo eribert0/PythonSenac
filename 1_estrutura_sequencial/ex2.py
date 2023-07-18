e189de4088fe3b529ac5b40f5c23a597e4d3e432
Eriberto Junior <43413666+eribert0@users.noreply.github.com>
2023-07-18 03:10:27 -0300

    Create ex2.py

---
+++ 1_estrutura_sequencial/ex2.py
@@ -0,0 +1,26 @@
+"""
+nome = 'Luiz Otavio'
+altura = 1.80
+peso = 95
+imc = peso / altura ** 2
+print(f"O imc do {nome} é {imc}")
+
+###########################################
+
+condicao1 = False
+condicao2 = False
+condicao3 = True
+condicao4 = True
+
+if condicao1 :
+    print('A condicao1 foi verdadeira')
+elif condicao2 :
+    print('A condicao2 foi verdadeira')
+elif condicao3 :
+    print('A condicao3 foi verdadeira')
+elif condicao4 :
+    print('A condicao2 foi verdadeira')
+else:
+    print('Nenhuma condicao é verdadeira')
+"""    
+#Estudando in e not in
