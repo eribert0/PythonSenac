7862b64ae86905c630bdc6e6555524b42091f215
Eriberto Junior <43413666+eribert0@users.noreply.github.com>
2023-07-19 03:35:20 -0300

    Update ex3.py

--- 2_estrutura_de_decisao/ex3.py
+++ 2_estrutura_de_decisao/ex3.py
@@ -1,7 +1,7 @@
 nome = input('Digite o primeiro nome: ')
 tamanho_nome = len(nome)
 if nome:
-    if tamanho_nome >=1 and <= 4:
+    if tamanho_nome >=1 and tamanho_nome <= 4:
         print('Seu nome é curto')
     elif tamanho_nome >= 5 and tamanho_nome <= 6:
         print('Seu nome é normal')
