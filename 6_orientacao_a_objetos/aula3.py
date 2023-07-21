6cbd9485cc15f5cc7864dc8309a1814f6b04b6df
Eriberto Junior <43413666+eribert0@users.noreply.github.com>
2023-07-21 01:38:20 -0300

    Create aula3.py

---
+++ 6_orientacao_a_objetos/aula3.py
@@ -0,0 +1,16 @@
+class Animal:
+    def __init__(self, nome):
+        self.nome = nome
+
+        variavel = 'valor'
+        print(variavel)
+    
+    def comendo(self, alimento):
+        return f'{self.nome} está comendo {alimento}'
+    
+    def executar(self, *args, **kwargs):
+        return self.comendo(*args, **kwargs)
+        
+leao = Animal(nome='leão')
+print(leao.nome)
+print(leao.executar('laranja'))
