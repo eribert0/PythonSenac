e4805db003e03328de496a53d16b3ae69ce93418
Eriberto Junior <43413666+eribert0@users.noreply.github.com>
2023-07-26 23:59:31 -0300

    Update cliente.py

--- 6_orientacao_a_objetos/revisandoConta.py/cliente.py
+++ 6_orientacao_a_objetos/revisandoConta.py/cliente.py
@@ -1,9 +1,13 @@
+from conta import Cliente
 class Cliente:
     def __init__(self, nome, snome, cpf, dataNasc):
         self.nome = nome
         self.sobrenome = snome
         self.cpf = cpf
         self.dataNasc = dataNasc
+
+    def dados_cliente(self):
+        return f'Nome: {self.nome}\n--------------------------\nSobrenome: {self.sobrenome}\n--------------------------\nCPF: {self.cpf}\n--------------------------\nNascimento: {self.dataNasc}\n--------------------------'
 
 c1 = Cliente('Eriberto', 'Junior', '123456789-10', '05/03/2000')
 print(f'Nome: {c1.nome}\n--------------------------\nSobrenome: {c1.sobrenome}\n--------------------------\nCPF: {c1.cpf}\n--------------------------\nNascimento: {c1.dataNasc}\n--------------------------')
