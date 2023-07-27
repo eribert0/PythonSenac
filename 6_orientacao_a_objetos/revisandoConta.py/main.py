abd51b0d1a8f05b97896104cb7bc430451cc53d2
Eriberto Junior <43413666+eribert0@users.noreply.github.com>
2023-07-26 23:59:59 -0300

    Update main.py

--- 6_orientacao_a_objetos/revisandoConta.py/main.py
+++ 6_orientacao_a_objetos/revisandoConta.py/main.py
@@ -1,3 +1,6 @@
 from cliente import Cliente
 from conta import Conta
 
+cliente_2 = Cliente(nome='Sarah', snome='Hillary', cpf='123456789-11', dataNasc='16/02/2005')
+
+print(cliente_2.dados_cliente())
