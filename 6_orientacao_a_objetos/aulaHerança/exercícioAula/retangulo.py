318c503eddb03ff78e5418095ab568b892eb4acf
Eriberto Junior <43413666+eribert0@users.noreply.github.com>
2023-08-03 17:19:21 -0300

    Create retangulo.py

---
+++ 6_orientacao_a_objetos/aulaHerança/exercícioAula/retangulo.py
@@ -0,0 +1,12 @@
+from forma import Forma
+
+class Retangulo(Forma):
+    def __init__(self):
+        super().__init__()
+        self.base = int
+        self.altura = int
+
+    def areaRetangulo(self):
+        print(f'Área: {self.base * self.altura}')
+    
+    
