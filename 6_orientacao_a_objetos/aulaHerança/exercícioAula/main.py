b830680884c95ecbf47523d0cb132bcc4de4a474
Eriberto Junior <43413666+eribert0@users.noreply.github.com>
2023-08-03 17:21:44 -0300

    Create main.py

---
+++ 6_orientacao_a_objetos/aulaHerança/exercícioAula/main.py
@@ -0,0 +1,10 @@
+from retangulo import Retangulo
+
+retangulo = Retangulo()
+retangulo.finalidade = 'peça'
+
+
+retangulo.base = int(input('Digite o valor da base: '))
+retangulo.altura = int(input('Digite o valor da altura: '))
+
+retangulo.areaRetangulo()
