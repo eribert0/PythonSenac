127fd6638179ee68423a4dd497e42267b0ad1d21
Eriberto Junior <43413666+eribert0@users.noreply.github.com>
2023-08-04 16:14:53 -0300

    Update gerente.py

--- 6_orientacao_a_objetos/aulaHerança/exercício1Aula/gerente.py
+++ 6_orientacao_a_objetos/aulaHerança/exercício1Aula/gerente.py
@@ -1,17 +1,17 @@
 from pessoa import Pessoa
 
-class Gerente:
+class Gerente(Pessoa):
     def __init__(self, nome, cpf, setor):
-        self.nome = nome
-        self.cpf = cpf
+        super().__init__(nome, cpf)
         self.setor = setor
         self.qtdEquipe = int
 
     def informacoes_Gerente(self):
-        print(f'Nome: {self.nome}\nCPF: {self.cpf}\nSetor: {self.setor}\n')
+        super().mostrar_informacoes(self)
+        print(f'Setor: {self.setor}\nQuantidade da equipe: {self.qtdEquipe}\n')
     
     def informacoes_bonificacao(self):
         if self.qtdEquipe >= 10:
-            salario += salario*0.95
+            return self.salario + (self.salario * 0.1)
         else:
-            salario += salario*0.95
+            return self.salario + (self.salario * 0.05)
