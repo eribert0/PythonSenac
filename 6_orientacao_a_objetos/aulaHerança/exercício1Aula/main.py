d51dce590df5c50973883f3500b6908450dd863e
Eriberto Junior <43413666+eribert0@users.noreply.github.com>
2023-08-04 16:14:50 -0300

    Update main.py

--- 6_orientacao_a_objetos/aulaHerança/exercício1Aula/main.py
+++ 6_orientacao_a_objetos/aulaHerança/exercício1Aula/main.py
@@ -1,16 +1,24 @@
 from funcionario import Funcionario
-#from gerente import Gerente
+from gerente import Gerente
 
 #Solicitando as informações ao usuário
-nome = input('Nome: ')
-cpf = input('CPF: ')
-cargo = input('Cargo: ')
+#nome = input('Nome: ')
+#cpf = input('CPF: ')
+#cargo = input('Cargo: ')
 
-funcionario1 = Funcionario(nome, cpf, cargo)
-funcionario1.salario = float(input('Digite o salário do funcionário: '))
-
-funcionario1.mostrar_informacoes()
+#f = Funcionario(nome, cpf, cargo)
+#f.salario = float(input('Digite o salário do funcionário: '))
+#f.mostrar_informacoes()
+"""
 horas = float(input('Quantidade de horas extras: '))
-funcionario1.calculo_horaExtra(horas)
+f.calculo_horaExtra(horas)
+f.informacoes_funcionario()
+"""
 
-funcionario1.informacoes_funcionario()
+nome_ger = input('Nome: ')
+cpf_ger = input('CPF:')
+setor_ger = input('Setor: ')
+
+g = Gerente(nome_ger, cpf_ger, setor_ger)
+g.qtdEquipe = int(input('Quantidade da equipe:'))
+g.informacoes_Gerente()
