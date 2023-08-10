8e0f138b5555973708975343c1026b10c33af963
Eriberto Junior <43413666+eribert0@users.noreply.github.com>
2023-08-10 14:46:24 -0300

    Update exercicioSenha.py

--- 6_orientacao_a_objetos/aulaExceção/exercicioSenha.py
+++ 6_orientacao_a_objetos/aulaExceção/exercicioSenha.py
@@ -1,12 +1,25 @@
-##### EXCEPTION ######
-
-
-######################
-
 class Usuario():
+
     def __init__(self, login, senha):
         self.login = login
         self.senha = senha
 
     def validar_senha(self):
-        
+        while True:
+            try:
+                if len(self.senha) < 8:
+                    raise ValueError('Senha deve ter pelo menos 8 caracteres')
+                elif not any(c.isupper() for c in self.senha):
+                    raise ValueError('Senha deve conter pelo menos uma letra maiúscula')
+                elif not any(c.isdigit() for c in self.senha):
+                    raise ValueError('Senha deve conter pelo menos um número')
+                else:
+                    print('Senha válida')
+                    break
+                    #self.senha = input('Digite uma senha de fácil memorização: ')
+            except ValueError as e:
+                print(f'Error! {e}')
+                self.senha = input('Digite novamente: ')
+
+u = Usuario(login=input('Login: '), senha=input('Senha: '))
+u.validar_senha()
